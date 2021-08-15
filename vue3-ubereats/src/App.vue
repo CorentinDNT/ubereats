ffe3d0a1235130871081eed44d9fdc6b69b4b29d
CorentinDnt <donatocorentin2@gmail.com>
2021-08-15 09:07:51 +0200

    start uber eats,
    function push restaurant

---
+++ vue3-ubereats/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app"></div>
+	<Home />
+</template>
+
+<script>
+import Home from "./pages/home.vue";
+export default {
+	name: "App",
+	components: {
+		Home,
+	},
+};
+</script>
+
+<style lang="scss">
+@import url("./style/reset.css");
+@import url("https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap");
+#app {
+	font-family: "Roboto", sans-serif;
+	padding: 20px;
+}
+</style>
---
+++ vue3-ubereats/src/pages/home.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="home">
+		<RestaurantRow />
+	</div>
+</template>
+
+<script>
+//IMPORTS
+import BDD from "../BDD.js";
+
+//COMPONENETS
+import RestaurantRow from "../components/RestaurantRow.vue";
+export default {
+	name: "Home",
+	components: {
+		RestaurantRow,
+	},
+	setup() {
+		class Restaurant {
+			constructor(name, note, image, drive_time) {
+				this.name = name;
+				this.note = note;
+				this.image = image;
+				this.drive_time = drive_time;
+			}
+		}
+		let dataRestaurant = [];
+
+		const makeDataRestaurant = () => {
+			let three_restaurant = [];
+
+			for (const restaurant of BDD) {
+				const new_restaurant = new Restaurant(
+					restaurant.name,
+					restaurant.note,
+					restaurant.image,
+					restaurant.drive_time
+				);
+
+				if (three_restaurant.length === 2) {
+					three_restaurant.push(new_restaurant);
+					dataRestaurant.push(three_restaurant);
+					three_restaurant = [];
+				} else {
+					three_restaurant.push(new_restaurant);
+				}
+			}
+			console.log(dataRestaurant);
+		};
+
+		makeDataRestaurant();
+	},
+};
+</script>
+
+<style></style>
